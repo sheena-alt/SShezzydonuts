4b00dae5adf2a9e5de2ae5d287099302dbc48ef4
sheena-alt <brillantessheenamay@gmail.com>
2026-04-18 23:00:06 +0800

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-// 1. Import Firestore SDK
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
